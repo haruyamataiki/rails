7469bd12a662a54ab20f7f8c31ccec70951d1c88
haruyamataiki <56719209+haruyamataiki@users.noreply.github.com>
2019-11-06 17:09:39 +0900

    [Finish]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e5b6ccf1d199d2756d27fd3f72602b17bfdebfd3fdd65632b84f73d03c713cf587a53af45f9da7c3dd6bf2d0d61acf6fc5c2f002871764be8e5c30800925a9a8'
